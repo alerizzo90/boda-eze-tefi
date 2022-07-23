8ee05ab00bb6231f3e9f1d0b9ff2bcd93e1633f0
alexisrizzo <alexis.rizzo21@gmail.com>
2022-07-22 23:25:15 -0300

    temporalizador

---
+++ app.js
@@ -0,0 +1,34 @@
+const $days = document.getElementById('dias'),
+$hours = document.getElementById('horas'),
+$minutes = document.getElementById('minutos'),
+$seconds = document.getElementById('segundos')
+
+
+//Fecha a futuro
+const countdownDate = new Date('2 11, 2023 19:00:00').getTime();
+
+let interval = setInterval(function(){
+    //Obtener fecha actual y milisegundos
+    const now = new Date().getTime();
+
+    //Obtener las distancias entre ambas fechas
+    let distance = countdownDate - now;
+
+    //Calculos a dias-horas-minutos-segundos
+    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((distance % (1000 * 60 * 60 * 24 )) / (1000 * 60 * 60));
+    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((distance % (1000 * 60 )) / (1000));
+
+    //Escribimos resultados
+    $days.innerHTML = days;
+    $hours.innerHTML = hours;
+    $minutes.innerHTML = minutes;
+    $seconds.innerHTML = ('0' + seconds).slice(-2);
+
+    //Cuando llegue a 0
+    if(distance < 0){
+        clearInterval(interval);
+        $finalMessage.style.transform = 'translateY(0)';
+    }
+}, 1000);
